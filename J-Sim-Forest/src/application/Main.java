355fda0f4b2c8bd94b6f91b32548cefa44f6ba39
amelle <amellehamouch@gmail.com>
2020-01-09 11:10:42 +0100

    Initialisation

---
+++ J-Sim-Forest/src/application/Main.java
@@ -0,0 +1,21 @@
+package application;
+	
+import javafx.application.Application;
+
+
+
+public abstract class Main extends Application {
+	
+	
+
+
+   
+
+
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
+}
